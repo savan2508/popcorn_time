60e631021c37b3f956f8da957e1cc58f15efb266
Savan <sawanpatel2508@gmail.com>
2023-12-15 11:22:40 -0500

    Add StarRating.jsx component.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { StarRating } from "./components/StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    {/*<App />*/}
+    <StarRating maxRating={5} />
+    <StarRating maxRating={10} />
+  </React.StrictMode>,
 );
