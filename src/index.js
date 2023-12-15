ede14ba9958f03c2392c830667ffe583b3a484dc
Savan <sawanpatel2508@gmail.com>
2023-12-15 16:03:02 -0500

    Add functionality to StarRating.jsx component

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/*<App />*/}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
     <StarRating maxRating={10} />
   </React.StrictMode>,
 );
